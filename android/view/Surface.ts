3c5e28583e73436cd4f49d49e0b8e20af8f9bb5e
linfaxin <linlinfaxin@163.com>
2015-10-22 18:12:23 +0800

    improve dirty draw

--- android/view/Surface.ts
+++ android/view/Surface.ts
@@ -22,14 +22,17 @@ module android.view{
         lockCanvas(dirty:Rect):Canvas{
             let fullWidth = this.mCanvasElement.offsetWidth;
             let fullHeight = this.mCanvasElement.offsetHeight;
-            let rect = new Rect(dirty);
+            let rect:Rect;
             if(dirty.isEmpty()){
-                rect.set(0, 0, fullWidth, fullHeight);
+                rect = new Rect(0, 0, fullWidth, fullHeight);
+            }else{
+                // +1/-1: more space, insure dirty area clear and draw ok
+                rect = new Rect(Math.floor(dirty.left-1), Math.floor(dirty.top-1), Math.ceil(dirty.right+1), Math.ceil(dirty.bottom+1));
             }
             let width = rect.width();
             let height = rect.height();
-            let canvas = Canvas.obtain(fullWidth, fullHeight);
-            canvas.clipRect(rect);
+            let canvas = Canvas.obtain(width, height);
+            canvas.translate(-rect.left, -rect.top);
 
             this.mLockedCanvasMap.set(canvas, rect);
 
@@ -48,7 +51,7 @@ module android.view{
                 //canvas.translate(rect.left, rect.top);
                 let mCanvasContent = this.mCanvasElement.getContext('2d');
                 //mCanvasContent.clearRect(rect.left, rect.top, canvas.getWidth(), canvas.getHeight());
-                mCanvasContent.drawImage(canvas.canvasElement, 0, 0);
+                mCanvasContent.drawImage(canvas.canvasElement, rect.left, rect.top);
                 //mCanvasContent.putImageData(canvas.canvasElement.getContext('2d').getImageData(rect.left, rect.top, rect.width(), rect.height()), rect.left, rect.top);
             }
             canvas.recycle();
