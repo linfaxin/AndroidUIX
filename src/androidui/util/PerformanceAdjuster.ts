cad87ef05faf7fe26ff05f410987e2e765263572
linfaxin <linlinfaxin@163.com>
2015-12-02 02:36:40 +0800

    show scroll bar in noCanvas mode

--- src/androidui/util/PerformanceAdjuster.ts
+++ src/androidui/util/PerformanceAdjuster.ts
@@ -1,12 +1,46 @@
 /**
  * Created by linfaxin on 15/12/1.
  */
+///<reference path="../../android/view/View.ts"/>
 ///<reference path="../../android/graphics/Canvas.ts"/>
 
 module androidui.util{
+    import Canvas = android.graphics.Canvas;
+    import Drawable = android.graphics.drawable.Drawable;
     export class PerformanceAdjuster {
+
         static noCanvasMode(){
             android.graphics.Canvas.prototype = HackCanvas.prototype;
+
+            android.view.View.prototype.onDrawVerticalScrollBar =
+                function(canvas:Canvas, scrollBar:Drawable, l:number, t:number, r:number, b:number):void {
+                    let scrollBarEl = this.bindElement['VerticalScrollBar'];
+                    if(!scrollBarEl){
+                        scrollBarEl = document.createElement('div');
+                        this.bindElement['VerticalScrollBar'] = scrollBarEl;
+                        scrollBarEl.style.zIndex = '9';
+                        scrollBarEl.style.position = 'absolute';
+                        scrollBarEl.style.background = 'black';
+                        this.bindElement.appendChild(scrollBarEl);
+                    }
+
+                    let height = b - t;
+                    let width = r - l;
+                    let size = height;
+                    let thickness = width;
+                    let extent = this.mScrollCache.scrollBar.mExtent;
+                    let range = this.mScrollCache.scrollBar.mRange;
+
+                    let length = Math.round( size * extent / range);
+                    let offset = Math.round( (size - length) * this.mScrollCache.scrollBar.mOffset / (range - extent));
+                    if(t<0) t = 0;
+                    if(offset<0) offset = 0;
+
+                    scrollBarEl.style.transform = scrollBarEl.style.webkitTransform = `translate(${l}px, ${t + offset}px`;
+                    scrollBarEl.style.width = (r-l)/2 + 'px';//half style
+                    scrollBarEl.style.height = length + 'px';
+                    scrollBarEl.style.opacity = this.mScrollCache.scrollBar.mVerticalThumb.getAlpha() / 255 + '';
+            };
         }
 
     }
