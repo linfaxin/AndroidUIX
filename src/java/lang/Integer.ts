e660dcba640222eec32d7fb0815595b468ee554b
linfaxin <linlinfaxin@163.com>
2016-04-26 16:40:24 +0800

    Integer max & min value change.

--- src/java/lang/Integer.ts
+++ src/java/lang/Integer.ts
@@ -3,8 +3,8 @@
  */
 module java.lang{
     export class Integer{
-        static MIN_VALUE = Number.MIN_SAFE_INTEGER;
-        static MAX_VALUE = Number.MAX_SAFE_INTEGER;
+        static MIN_VALUE = -0x80000000;
+        static MAX_VALUE = 0x7fffffff;
 
         static parseInt(value:string):number {
             return Number.parseInt(value);
